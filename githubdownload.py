8d2ba88d4230d4440cd54f0c1257107c42ce92c4
Edouard GARNIER <edouard.garnier@viacesi.fr>
2025-10-06 18:26:44 +0200

    Simplified the github downloader

--- githubdownload.py
+++ githubdownload.py
@@ -1,16 +1,13 @@
 import requests
 
-
-def download_github_zip(owner, repo, branch='main', output_filename=None):
+def download_github_zip(owner, repo, branch='main'):
     url = f"https://github.com/{owner}/{repo}/archive/refs/heads/{branch}.zip"
-    if output_filename is None:
-        output_filename = f"{repo}-{branch}.zip"
-    response = requests.get(url, stream=True)
+    output_filename = f"{repo}-{branch}.zip"
+    response = requests.get(url)
     response.raise_for_status()
     with open(output_filename, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-    print(f"Fichier téléchargé : {output_filename}")
+        f.write(response.content)
+    print(f"Fichier téléchargé : {output_filename}")
 
 # Test
-download_github_zip('Pulongon', 'Shei-Pa-National-Park-Study', 'main')
+download_github_zip('Pulongon', 'Shei-Pa-National-Park-Study')

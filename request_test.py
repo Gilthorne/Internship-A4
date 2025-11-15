7282171753190d2571914d1f529cc7a85cd0ad69
Edouard GARNIER <edouard.garnier@viacesi.fr>
2025-11-15 09:56:13 +0100

    Basic changes

--- request_test.py
+++ request_test.py
@@ -3,9 +3,12 @@ import json
 
 API_KEY = '5e0c4b89c3dc998fda16c52f50e7f4a2'
 
+
 response = requests.get(
     "https://api.elsevier.com/content/article/pii/S1574954125004352",
-    headers={'X-ELS-APIKey': API_KEY}
+    headers={'X-ELS-APIKey': API_KEY, 'Accept': 'application/json'},
 )
 
+
+print(b'Data availability' in response.content)
 print (response.status_code)

63ec78ad3556bd7d6cbe1bcb8730f3e084614158
Edouard GARNIER <edouard.garnier@viacesi.fr>
2025-11-15 10:40:42 +0100

    Env done, better this way

--- request_test.py
+++ request_test.py
@@ -1,12 +1,28 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
+import mysql.connector as sql
 
-API_KEY = '5e0c4b89c3dc998fda16c52f50e7f4a2'
 
+load_dotenv()
+
+
+input_files = open('ResearchTestLinks.json')
+json_decode=json.load(input_files)
+
+db_connect=sql.connect(
+    user = os.getenv('DB_USER'),
+    password = os.getenv('DB_PASSWORD'),
+    host = os.getenv('DB_HOST'),
+    database = os.getenv('DB_NAME'),
+    port = int(os.getenv('DB_PORT'))
+)
 
 response = requests.get(
     "https://api.elsevier.com/content/article/pii/S1574954125004352",
-    headers={'X-ELS-APIKey': API_KEY, 'Accept': 'application/json'},
+    headers={'X-ELS-APIKey': os.getenv('ELSEVIER_API_KEY'), 
+             'Accept': 'application/json'}
 )
 
 
